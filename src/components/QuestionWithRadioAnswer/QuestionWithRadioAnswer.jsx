f750f40e25e72cdcdbe46544b016a4a098b21abe
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-03-12 10:25:04 -0700

    - created component for question with answers - radio buttons

---
+++ src/components/QuestionWithRadioAnswer/QuestionWithRadioAnswer.jsx
@@ -0,0 +1,44 @@
+import Button from "../Button/Button";
+import {useEffect, useState} from "react";
+import './QuestionWithRadioAnswer.css';
+
+function QuestionWithRadioAnswer({question, possibleAnswers, setAnswers, currentQuestion, answers}) {
+    const [selectedAnswer, setSelectedAnswer] = useState('');
+
+    useEffect(() => {
+        setSelectedAnswer(answers[currentQuestion]);
+    }, [currentQuestion, answers]);
+
+    console.log('Current Question:', currentQuestion); // Add this
+    console.log('Selected answer:', selectedAnswer); // Add this
+
+    const handleAnswerClick = (answer) => {
+        setSelectedAnswer(answer.text);
+        setAnswers(prevAnswers => ({
+            ...prevAnswers,
+            [currentQuestion]: answer.text
+        }));
+    };
+
+    return (
+        <div className={"question-body"}>
+            <h1 className={"question"}>{question}</h1>
+            <div className={"answers"}>
+                {possibleAnswers.map((answer, index) => (
+                    <div className={"button-div"}>
+                        <Button
+                            key={index}
+                            onClick={() => handleAnswerClick(answer)}
+                            className={answer.text === selectedAnswer ? 'selected' : ''}
+                        >
+                            {answer.image && <img className={"question-img"} src={answer.image} alt={answer.text}/>}
+                            {answer.text}
+                        </Button>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default QuestionWithRadioAnswer;
