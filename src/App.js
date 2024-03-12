6a8db573856935e13b6f26453aef8939cdc623af
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-03-12 13:45:09 -0700

    renamed thank you page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import QuestionPage from './pages/QuestionPage/QuestionPage';
-import ThankYouPage from './pages/ThankYouPage';
+import ThankYouPage from './pages/ThankTouPage/ThankYouPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -17,7 +17,7 @@ function App() {
           <Routes>
               <Route exact path="/" element={<HomePage />} />
               <Route path="/questions" element={<QuestionPage />} />
-              <Route path="/thankyou" element={<ThankYouPage />} />
+              <Route path="/thank-you" element={<ThankYouPage />} />
           </Routes>
         <Footer />
       </Router>
