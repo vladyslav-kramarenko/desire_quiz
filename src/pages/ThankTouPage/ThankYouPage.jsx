c9ba0c24fd68db65c80d47a804f35b680120ff47
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-03-12 13:49:35 -0700

    added thank you page

---
+++ src/pages/ThankTouPage/ThankYouPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./ThankYouPage.css";
+const ThankYouPage = () => {
+    return (
+        <main className="thank-you-page">
+            <div className="background-image">
+                <h1 className="title">Дякуэмо за заявку</h1>
+                <p className="subtitle">Менеджер зв'яжеться з вами найближчим часом</p>
+                <a className={"visit-website-link"} href={"https://desire-antalya.com"} >
+                    <button className={"visit-website-btn orange-btn"}>Відвідати сайт</button>
+                    </a>
+            </div>
+        </main>
+    );
+};
+
+export default ThankYouPage;
