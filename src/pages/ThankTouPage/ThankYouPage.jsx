15a8fefe9d43d266d7cc7eb8b12f10b426282d2d
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-03-13 03:36:36 -0700

    text fix added utm tags to link

--- src/pages/ThankTouPage/ThankYouPage.jsx
+++ src/pages/ThankTouPage/ThankYouPage.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import "./ThankYouPage.css";
 const ThankYouPage = () => {
+    function getUtmUrl() {
+        let url = "https://desire-antalya.com/uk/";
+        const params = {
+            utm_source: localStorage.getItem('utm_source'),
+            utm_medium: localStorage.getItem('utm_medium'),
+            utm_campaign: localStorage.getItem('utm_campaign'),
+            utm_term: localStorage.getItem('utm_term'),
+            utm_content: localStorage.getItem('utm_content')
+        };
+        const urlParams = new URLSearchParams(params);
+        if(urlParams.toString()) {
+            url += "?" + urlParams.toString();
+        }
+        return url;
+    }
+
     return (
         <main className="thank-you-page">
             <div className="background-image">
-                <h1 className="title">Дякуємо за заяву</h1>
+                <h1 className="title">Дякуємо за заявку</h1>
                 <p className="subtitle">Менеджер зв'яжеться з вами найближчим часом</p>
-                <a className={"visit-website-link"} href={"https://desire-antalya.com/uk/"} >
+                <a className={"visit-website-link"} href={getUtmUrl()} >
                     <button className={"visit-website-btn orange-btn"}>Відвідати сайт</button>
                     </a>
             </div>
