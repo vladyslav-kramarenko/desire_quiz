7679a3168fb6c65cd0f8404ce3cf047215259907
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-03-13 03:05:13 -0700

    text fix

--- src/pages/ThankTouPage/ThankYouPage.jsx
+++ src/pages/ThankTouPage/ThankYouPage.jsx
@@ -4,7 +4,7 @@ const ThankYouPage = () => {
     return (
         <main className="thank-you-page">
             <div className="background-image">
-                <h1 className="title">Дякуэмо за заявку</h1>
+                <h1 className="title">Дякуємо за заявку</h1>
                 <p className="subtitle">Менеджер зв'яжеться з вами найближчим часом</p>
                 <a className={"visit-website-link"} href={"https://desire-antalya.com"} >
                     <button className={"visit-website-btn orange-btn"}>Відвідати сайт</button>
