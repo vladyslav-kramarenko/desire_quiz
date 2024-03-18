d3be90ed5e612fda287f54ef5c34437eedc05b80
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-03-18 13:10:37 -0700

    added google tag manager

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals';import insertGtmScript from './Gtm';
+
+insertGtmScript();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
