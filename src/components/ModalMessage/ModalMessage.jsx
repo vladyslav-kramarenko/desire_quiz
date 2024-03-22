40f51e0eed5064a1a428933e4f25f3d2db4a65fe
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-03-21 20:50:43 -0700

    added localization to modal component

--- src/components/ModalMessage/ModalMessage.jsx
+++ src/components/ModalMessage/ModalMessage.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import './ModalMessage.css';
+import {useIntl} from "react-intl";
 
 const ModalMessage = ({ isOpen, message, onClose }) => {
+    const intl = useIntl();
     if (!isOpen) return null;
 
     return (
         <div className="modal-overlay" onClick={onClose}>
             <div className="modal-body" onClick={e => e.stopPropagation()}>
                 <h1>{message}</h1>
-                <button onClick={onClose}>Close</button>
+                <button onClick={onClose}>
+                    {intl.formatMessage({id: 'closeModal'})}
+                </button>
             </div>
         </div>
     );
