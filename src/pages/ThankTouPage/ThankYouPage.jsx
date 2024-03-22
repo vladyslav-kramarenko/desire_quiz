5c8df0294f4f99a194ad6af5f1413f5d7c5789e6
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-03-21 19:24:47 -0700

    added localization to thank you page

--- src/pages/ThankTouPage/ThankYouPage.jsx
+++ src/pages/ThankTouPage/ThankYouPage.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import "./ThankYouPage.css";
+import {useIntl} from "react-intl";
 const ThankYouPage = () => {
+    const intl = useIntl();
     function getUtmUrl() {
-        let url = "https://desire-antalya.com/uk/";
+        const languagePath = {
+            en: '',
+            ua: '/uk',
+            ru: '/ru',
+            tr: '/tr',
+        };
+        let basePath = `https://desire-antalya.com${languagePath[intl.locale] || ''}/`;
+
         const params = {
             utm_source: localStorage.getItem('utm_source'),
             utm_medium: localStorage.getItem('utm_medium'),
@@ -11,20 +20,21 @@ const ThankYouPage = () => {
             utm_content: localStorage.getItem('utm_content')
         };
         const urlParams = new URLSearchParams(params);
-        if(urlParams.toString()) {
-            url += "?" + urlParams.toString();
-        }
-        return url;
+
+        return basePath + (urlParams.toString() ? "?" + urlParams.toString() : '');
     }
 
     return (
         <main className="thank-you-page">
             <div className="background-image">
-                <h1 className="title">Дякуємо за заявку</h1>
-                <p className="subtitle">Менеджер зв'яжеться з вами найближчим часом</p>
-                <a className={"visit-website-link"} href={getUtmUrl()} >
-                    <button className={"visit-website-btn orange-btn"}>Відвідати сайт</button>
-                    </a>
+                <h1 className="title">{intl.formatMessage({id: 'thankYouTitle'})}</h1>
+                <p className="subtitle">{intl.formatMessage({id: 'thankYouSubtitle'})}</p>
+                <a className="visit-website-link" href={getUtmUrl()}>
+                    <button
+                        className="visit-website-btn orange-btn">
+                        {intl.formatMessage({id: 'visitSiteButton'})}
+                    </button>
+                </a>
             </div>
         </main>
     );
